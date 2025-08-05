0949a3379151e11ab787b62e5556c03b32dacfc4
Rishabh Pandey <v-pandeyrish@microsoft.com>
2025-08-05 19:40:25 +0530

    Update aggregation.py
    Anurag changes - iteration object added in propmtflow

--- backend/PromptFlow/legal-main-flow/aggregation.py
+++ backend/PromptFlow/legal-main-flow/aggregation.py
@@ -1,15 +1,15 @@
-
 from promptflow.core import tool
 import logging
-
+ 
 # The inputs section will change based on the arguments of the tool function, after you save the code
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
 @tool
-def my_python_tool(input1: object, input2: object, input3: object, input4: object, input5: object,input6: object) -> object:
-
+#def my_python_tool(input1: object, input2: object, input3: object, input4: object, input5: object) -> object:
+def my_python_tool(input1: object, input2: object, input3: object, input4: object, input5: object, input6: object) -> object:
+ 
     #check witch input is not null and return it
-    
+   
     if input1:
         return input1
     elif input2:
@@ -21,7 +21,7 @@ def my_python_tool(input1: object, input2: object, input3: object, input4: objec
     elif input5 == False or input5 == True:
         out = {"Found": input5}
         return out
+    elif input6:
+        return input6
     else:
-        return "All inputs are null!"
-
-    
+        return "All inputs are null"

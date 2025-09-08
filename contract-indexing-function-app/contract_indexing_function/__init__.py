09bc9a1b23b3a79cba45ecf048de1e85278e8c3a
Rishabh Pandey <v-pandeyrish@microsoft.com>
2025-09-08 23:12:24 +0530

    Update __init__.py
    Updated for removing blob reference.

--- contract-indexing-function-app/contract_indexing_function/__init__.py
+++ contract-indexing-function-app/contract_indexing_function/__init__.py
@@ -4,12 +4,15 @@ import json
 from . import contract_indexing
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Indexing function is triggered.") 
+    logging.info("Contract indexing is triggered.")
 
 ## fetch input - filename from http request's json body
     try:
-        logging.info("Inside process_all_documents.")
-        return_value = contract_indexing.process_documents_from_blob()
+        req_body = req.get_json()
+        filename = req_body.get("filename")
+        filecontent = req_body.get("filecontent")
+
+        return_value = contract_indexing.process_documents_from_blob(filename, filecontent)
         
         if return_value=="good":
             print("return value is passed as good")
@@ -17,7 +20,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse(f"Document prepared.", status_code=200)
         else:
             logging.info("Error in function - process_documents.")
-            return func.HttpResponse(f"{return_value}", status_code=500)
+            return func.HttpResponse(f"Bad response!", status_code=500)
     except Exception as e:
         logging.info("Error in executing function app.")
-        return func.HttpResponse(f"Error in running Function app.", status_code=500)
+        return func.HttpResponse(f"Error in running Function app", status_code=500)
